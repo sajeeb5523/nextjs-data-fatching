9094e07328b27dca5a29ac06671e7cf5f1637f96
sajeeb5523 <sajeebaljabed1@gmail.com>
2025-08-17 19:24:04 +0600

    posts create and posts data fetch done

---
+++ src/app/posts/page.jsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import React from 'react'
+
+export const getPosts = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
+    return data;
+}
+
+export default async function Posts() {
+
+    const posts = await getPosts();
+
+    return (
+        <div>
+            {posts.map((singlePost) => {
+                return (
+                    <div key={singlePost.id} className='border-2 border-gray-300 space-y-3 p-3 my-3 rounded-2xl'>
+                        <p>User-id: {singlePost.id}</p>
+                        <p>Title: {singlePost.title}</p>
+                        <p>Body: {singlePost.body}</p>
+                    </div>
+                );
+            })}
+        </div>
+    )
+}
