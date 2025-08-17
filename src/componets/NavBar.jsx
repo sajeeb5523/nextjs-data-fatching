5549daa906a9405ae4698faed09d5c5943df4c28
sajeeb5523 <sajeebaljabed1@gmail.com>
2025-08-17 19:15:18 +0600

    navbar done

---
+++ src/componets/NavBar.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <div>
+            <nav>
+                <ul className='flex justify-around items-center bg-gray-800 text-white p-4'>
+                    <Link href="/"><li>Home</li></Link>
+                    <Link href="/posts"><li>Posts</li></Link>
+                    <Link href="/comments"><li>Comments</li></Link>
+                </ul>
+            </nav>
+        </div>
+    )
+}
