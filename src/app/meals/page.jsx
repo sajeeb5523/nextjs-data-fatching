d0810839acf070cd7ac293acf7917426ac4abf56
sajeeb5523 <sajeebaljabed1@gmail.com>
2025-08-24 22:28:08 +0600

    Image add from next/image

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,6 +1,7 @@
 
 import Link from "next/link";
 import MealSearchInput from "./components/MealSearchInput";
+import Image from "next/image";
 
 export const metadata = {
     title: "All Meals",
@@ -32,7 +33,13 @@ export default async function MealsPage({ searchParams }) {
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-4'>
                 {meals.map((singleMeal) => (
                     <div key={singleMeal.idMeal} className='border border-gray-300 p-4 space-y-5 rounded-xl'>
-                        <img src={singleMeal.strMealThumb} alt={singleMeal.strMeal} className="w-full h-48 object-cover rounded" />
+                        <Image
+                            src={singleMeal.strMealThumb}
+                            alt={singleMeal.strMeal}
+                            width={641}
+                            height={621}
+                            className="w-full h-48 object-cover rounded"
+                        />
                         <p className='text-2xl font-bold'>{singleMeal.strMeal}</p>
                         <p>{singleMeal.strInstructions}</p>
                         <Link href={`/meals/${singleMeal.idMeal}`}>Details</Link>
